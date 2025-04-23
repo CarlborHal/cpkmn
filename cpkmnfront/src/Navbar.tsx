cdc0ac94064defe75a8ab2eb2271a0c149d2c1ce
CarlborHal <grwlp5k@gmail.com>
2025-04-23 12:19:00 -0400

    about to start auth

--- cpkmnfront/src/Navbar.tsx
+++ cpkmnfront/src/Navbar.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 // import HektarB from './hektarbattle.mp3';
 
 export default function Navbar() {
-  console.log('navbar rendered');
+  // console.log('navbar rendered');
   return (
     <nav>
       <Link to="/">Customizer</Link>
