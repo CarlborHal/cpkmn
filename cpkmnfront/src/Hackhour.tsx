e90d432bfd5996f182375fefb762fff94eb4cb12
CarlborHal <grwlp5k@gmail.com>
2025-04-23 21:34:32 -0400

    did hack hour but at what cost

--- cpkmnfront/src/Hackhour.tsx
+++ cpkmnfront/src/Hackhour.tsx
@@ -2,30 +2,55 @@ import { useState } from 'react';
 
 export default function Hackhour() {
   const [num, setNum] = useState<number>(0);
-  const [arr, setArr] = useState<number[]|string>([]);
+  const [arr, setArr] = useState<string>('');
+  const [ans, setAns]= useState<boolean | null| string>('placeholder')
 
   function handleChange(event:React.FormEvent<HTMLFormElement>) {
-    if (event.target.id === 'number') {
-        setNum(event.target.value)
-        console.log(num)
-    } else if (event.target.id === 'array') {
+    const target = event.target as HTMLInputElement;
+    if (target.id === 'number') {
+        setNum(Number(target.value))
 
-        setArr(event.target.value)
-        console.log(arr)
+
+    } else if (target.id === 'array') {
+
+        setArr(target.value)
+
     }
+
   }
-  function handleSubmit(event){
-    console.log(event)
-    console.log(num)
-    console.log(arr)
+  function handleSubmit(event:React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+
+    const realNum = Number(num) as number;
+    const realArr:number[]=JSON.parse(arr as string)
+
+
+
+    // console.log(Array.isArray(realArr))
+    // const newArr= realArr.trim()
+    let l= 0;
+    let r = realArr.length-1;
+    console.log(realArr)
+    while (l<=r){
+        const m = Math.floor((l+r)/2)
+        if (realArr[m]===realNum)
+            { setAns(true)
+                return}
+        else if (realArr[m]>realNum) r=m-1//5 looking for 2, move r
+        else if (realArr[m]<realNum) l=m+1
+    }
+    setAns(false)
+    return
   }
   return (
     <div>
       <form onChange={handleChange} onSubmit={handleSubmit}>
-      <input type='text' name='number' id = 'number'></input>
-      <input type='text' name='array' id = 'array'></input>
+      <input type='text' name='number' id = 'number' placeholder='number'></input>
+      <input type='text' name='array' id = 'array' placeholder ='array'></input>
       <button type='submit'>submit</button>
       </form>
+      {String(ans)}
+
     </div>
   );
 }
